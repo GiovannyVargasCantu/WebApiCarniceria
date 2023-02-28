a81aab7622ca866e9828115fa40209c194c7d434
GiovannyVargasCantu <81483412+GiovannyVargasCantu@users.noreply.github.com>
2023-02-27 21:24:07 -0600

    Actividad 1.11 de Nexus
    En esta parte realice la actiivdad 1.11 de Nexus, y dejo el trabajo aqui para posteriores actividades.

---
+++ WebApiCarniceria/WebApiCarniceria/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiCarniceria.Entidades;
+
+namespace WebApiCarniceria
+
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Producto> Producto { get; set; }
+    }
+    
+}
---
+++ WebApiCarniceria/WebApiCarniceria/Entidades/Carniceria.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiCarniceria.Entidades
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        
+        public string Nombre { get; set; }  
+    }
+}
